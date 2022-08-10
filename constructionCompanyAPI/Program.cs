8ef4eb19c9e36e28695958485051a01f1db45ae4
bartek101119 <bartek101119@wp.pl>
2022-08-10 13:01:19 +0200

    the startup class deleted (version 6.0)

--- constructionCompanyAPI/Program.cs
+++ constructionCompanyAPI/Program.cs
@@ -1,27 +1,121 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using NLog.Web;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using constructionCompanyAPI.Authorization;
+using constructionCompanyAPI.Entities;
+using constructionCompanyAPI.Middleware;
+using constructionCompanyAPI.Models;
+using constructionCompanyAPI.Models.Validators;
+using constructionCompanyAPI.Services;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using constructionCompanyAPI;
+using System.Reflection;
 
-namespace constructionCompanyAPI
+var builder = WebApplication.CreateBuilder();
+
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
+
+var authenticationSettings = new AuthenticationSettings();
+
+builder.Services.AddSingleton(authenticationSettings);
+
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+builder.Services.AddAuthentication(option =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                }).UseNLog();
-    }
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authenticationSettings.JwtIssuer,
+        ValidAudience = authenticationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+
+    };
+
+});
+// w³asna polityka autoryzacji
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("HasNationality", builder => builder.RequireClaim("Nationality"));
+    options.AddPolicy("Atleast18", builder => builder.AddRequirements(new MinimumAgeRequirement(18)));
+    options.AddPolicy("AtLeast2Companies", builder => builder.AddRequirements(new MinimumNumberOfCompanies(2)));
+});
+
+builder.Services.AddScoped<IAuthorizationHandler, MinimumNumberOfCompaniesHandler>();
+builder.Services.AddScoped<IAuthorizationHandler, ResourceOperationRequirementHandler>();
+builder.Services.AddScoped<IAuthorizationHandler, MinimumAgeRequirementHandler>();
+builder.Services.AddControllers().AddFluentValidation();
+builder.Services.AddScoped<ConstructionCompanySeeder>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddScoped<IConstructionCompanyService, ConstructionCompanyService>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<RequestTimeMiddleware>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+builder.Services.AddScoped<IUserContextService, UserContextService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<IValidator<ConstructionCompanyQuery>, ConstructionCompanyQueryValidator>();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("FrontEndClient", policyBuilder =>
+         policyBuilder.AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins(builder.Configuration["AllowedOrigins"])
+
+     );
+});
+builder.Services.AddDbContext<ConstructionCompanyDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConstructionCompanyDb")));
+
+var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<ConstructionCompanySeeder>();
+
+app.UseResponseCaching();
+app.UseStaticFiles();
+app.UseCors("FrontEndClient");
+
+seeder.Seed();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseMiddleware<RequestTimeMiddleware>();
+app.UseAuthentication();
+
+app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ConstructionCompany API");
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
