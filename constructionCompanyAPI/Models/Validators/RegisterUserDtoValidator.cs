b95d6a317551702d6fc19ee686e27838adf64a0b
Bartosz Michalski <b.michalski@ideo.pl>
2023-08-17 10:44:45 +0200

    [Validator] More secure password validation added

--- constructionCompanyAPI/Models/Validators/RegisterUserDtoValidator.cs
+++ constructionCompanyAPI/Models/Validators/RegisterUserDtoValidator.cs
@@ -9,26 +9,48 @@ namespace constructionCompanyAPI.Models.Validators
 {
     public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
     {
-        
-
         public RegisterUserDtoValidator(ConstructionCompanyDbContext dbContext)
         {
             RuleFor(x => x.Email).NotEmpty().EmailAddress();
 
-            RuleFor(x => x.Password).MinimumLength(6);
+            RuleFor(x => x.Password).Custom((password, context) =>
+            {
+                if (password.Length < 8)
+                {
+                    context.AddFailure(nameof(RegisterUserDto.Password), "Password must be at least 8 characters long.");
+                }
+                else if (!ContainsRequiredCharacters(password))
+                {
+                    context.AddFailure(nameof(RegisterUserDto.Password), "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+                }
+            });
 
             RuleFor(x => x.ConfirmPassword).Equal(p => p.Password);
 
-            RuleFor(x => x.Email).Custom((value, context) => 
+            RuleFor(x => x.Email).Custom((value, context) =>
             {
                 var emailInUse = dbContext.Users.Any(x => x.Email == value);
                 if (emailInUse)
                 {
                     context.AddFailure("Email", "That email is taken");
                 }
-            
+
             });
-           
+
+        }
+        private bool ContainsRequiredCharacters(string password)
+        {
+            return
+                password.Any(char.IsUpper) &&
+                password.Any(char.IsLower) &&
+                password.Any(char.IsDigit) &&
+                password.Any(IsSpecialCharacter);
+        }
+
+        private bool IsSpecialCharacter(char c)
+        {
+            var specialCharacters = new[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')' };
+            return specialCharacters.Contains(c);
         }
     }
 }
